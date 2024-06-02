612f6251d890a68245b1d979e75ff17d48a960ac
Fatimah Az Zahra <140265471+ftmhazzahra@users.noreply.github.com>
2024-06-02 14:07:43 +0700

    Add files via upload

---
+++ Modul9/src/Projek/koneksi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Projek;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+    private String url= "jdbc:mysql://localhost:4306/dbproyek"; 
+    private String username_xampp = "root";
+    private String password_xampp = "";
+    private Connection con;
+    
+    public void connect(){
+        try{
+            con = DriverManager.getConnection(url, username_xampp, password_xampp);
+            System.err. println("koneksi berhasil");
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    public Connection getCon(){
+        if (con == null) {
+            connect();
+        }
+        return con;
+    }
+}
